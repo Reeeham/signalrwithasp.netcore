cd84570a4644b8f4f2b0a79b625ed838d9c00add
@reeeham <reeehamgamal@gmail.com>
2021-06-06 05:44:39 +0200

    initial commit

---
+++ TheCallCenter/Hubs/CallCenterHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using TheCallCenter.Data.Entities;
+
+namespace TheCallCenter.Hubs
+{
+  public class CallCenterHub : Hub<ICallCenterHub>
+  {
+    public async Task NewCallReceived(Call newCall)
+    {
+      await Clients.Group("CallCenters").NewCallReceived(newCall);
+    }
+
+    public async Task JoinCallCenters()
+    {
+      await Groups.AddToGroupAsync(Context.ConnectionId, "CallCenters");
+    }
+  }
+}
